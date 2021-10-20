f4739e12f64f2a6222c3e7f7a8a4e5b8dfb99eea
daniellfreelancer <daniellugofreelancer@gmail.com>
2021-10-20 20:38:54 -0300

    acción javaScript consultar nombre a potencial cliente

---
+++ js/javaScript.js
@@ -0,0 +1,5 @@
+// Desafio #1 JS
+
+var cliente = prompt("Bienvenidos a Sofia Cocina ¿ Cuál es tu nombre ?");
+
+alert ("Bienvenido " + cliente + " " +"a nuestra tienda, es un gusto tenerte como cliente");
