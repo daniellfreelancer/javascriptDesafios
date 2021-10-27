2d251c7c7a1f3320489aed9cbbd67c2250574605
daniellfreelancer <daniellugofreelancer@gmail.com>
2021-10-27 09:24:11 -0300

    Desafio #2 Javascript

--- js/javaScript.js
+++ js/javaScript.js
@@ -1,5 +1,22 @@
 // Desafio #1 JS
 
-var cliente = prompt("Bienvenidos a Sofia Cocina ¿ Cuál es tu nombre ?");
+// var cliente = prompt("Bienvenidos a Sofia Cocina ¿ Cuál es tu nombre ?");
 
-alert ("Bienvenido " + cliente + " " +"a nuestra tienda, es un gusto tenerte como cliente");
+// alert ("Bienvenido " + cliente + " " +"a nuestra tienda, es un gusto tenerte como cliente");
+
+
+// Desafio #2 JS
+
+
+let userAdmin = prompt("Bienvenido: ingrese usuario");
+let userPass = prompt("Bienvenido: ingrese su clave");
+
+if ((userAdmin == "admin") && (userPass == "admin")) {
+    alert ( "Bienvenido administrador" )
+} else {
+    alert ( "Usuario o clave incorrecta")
+}
+
+
+
+
